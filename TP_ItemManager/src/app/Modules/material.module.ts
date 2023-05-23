ab373f3b08b528313993f2009456aa9b6f7192a5
michaellaros <56259544+michaellaros@users.noreply.github.com>
2023-05-23 15:36:23 +0200

    Create material.module.ts

---
+++ TP_ItemManager/src/app/Modules/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+const MaterialComponents = [
+  MatButtonModule,
+  MatSlideToggleModule,
+  MatButtonToggleModule,
+  MatCheckboxModule
+  ,MatToolbarModule,
+  MatCardModule,
+  MatDividerModule,
+  MatIconModule
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports:[MaterialComponents]
+})
+export class MaterialModule { }

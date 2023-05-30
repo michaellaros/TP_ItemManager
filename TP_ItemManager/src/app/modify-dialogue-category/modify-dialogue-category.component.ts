9aa27a415108ff348a7b0ded7691ca8ecd35c98d
MixterKill29 <mixter.kill29@gmail.com>
2023-05-30 10:28:38 +0200

    fix modale e stili minori

--- TP_ItemManager/src/app/modify-dialogue-category/modify-dialogue-category.component.ts
+++ TP_ItemManager/src/app/modify-dialogue-category/modify-dialogue-category.component.ts
@@ -8,48 +8,44 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-modify-dialogue-category',
   templateUrl: './modify-dialogue-category.component.html',
-  styleUrls: ['./modify-dialogue-category.component.scss']
+  styleUrls: ['./modify-dialogue-category.component.scss'],
 })
 export class ModifyDialogueCategoryComponent {
+  category!: Category;
 
-  category!:Category
-
-
-  constructor( @Inject(MAT_DIALOG_DATA) private data: Category,
-    public dialogRef: MatDialogRef<ModifyDialogueCategoryComponent>
-    ,private http: HttpService,public status:StatusService) {
-      {
-        this.category = this.data;
-      }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: Category,
+    public dialogRef: MatDialogRef<ModifyDialogueCategoryComponent>,
+    private http: HttpService,
+    public status: StatusService
+  ) {
+    {
+      this.category = this.data;
     }
-    categoryForm= new FormGroup({
-      avaible:new FormControl(false),
-      codConsumationAllowed: new FormControl(''),
-      id:new FormControl(''),
-      imagePath:new FormControl(''),
-      name:new FormControl(''),
-      dineIn:new FormControl(false),
-      takeAway:new FormControl(false)
-    })
+  }
+  categoryForm = new FormGroup({
+    available: new FormControl(false),
+    codConsumationAllowed: new FormControl(''),
+    id: new FormControl(''),
+    imagePath: new FormControl(''),
+    name: new FormControl(''),
+    dineIn: new FormControl(false),
+    takeAway: new FormControl(false),
+  });
 
-    ngOnInit() {
-      if(this.category != null)
-      {
-        this.categoryForm.patchValue({
-          avaible: this.category.avaible,
-          codConsumationAllowed:this.category.codConsumationAllowed,
-          id:this.category.id?.toString(),
-          imagePath:this.category.imagePath,
-          name:this.category.name,
-          dineIn: this.category.codConsumationAllowed?.includes('DI'),
-          takeAway: this.category.codConsumationAllowed?.includes('TA')
-
-        })
-      }
+  ngOnInit() {
+    if (this.category != null) {
+      this.categoryForm.patchValue({
+        available: this.category.avaible,
+        codConsumationAllowed: this.category.codConsumationAllowed,
+        id: this.category.id?.toString(),
+        imagePath: this.category.imagePath,
+        name: this.category.name,
+        dineIn: this.category.codConsumationAllowed?.includes('DI'),
+        takeAway: this.category.codConsumationAllowed?.includes('TA'),
+      });
     }
+  }
 
-    confirmObject(){
-
-    }
-
+  confirmObject() {}
 }

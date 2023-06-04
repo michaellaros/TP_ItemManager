c3699ffa2ae4d1907d53ce1cac8660f62f72f103
MixterKill29 <mixter.kill29@gmail.com>
2023-06-04 22:37:16 +0200

    fix margin

--- TP_ItemManager/src/app/Pages/translations-editor/translations-editor.component.ts
+++ TP_ItemManager/src/app/Pages/translations-editor/translations-editor.component.ts
@@ -31,39 +31,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
     ]),
   ],
 })
-
-
 export class TranslationsEditorComponent {
-  public valueTranslation: string[] = ["Name", "Description","Preview"];
-   public fieldTranslation: string[] = ["EL", "IT","EN"]
+  public valueTranslation: string[] = ['Name', 'Description', 'Preview'];
+  public fieldTranslation: string[] = ['EL', 'IT', 'EN'];
   @Input() public translations?: Translation[];
   public translation?: Translation;
   @Input() flg_isEditable!: boolean;
   @Input() id!: string;
   @Input() type!: string;
-  public state: boolean = false;
+  public state: boolean = true;
   public newTranslation: Translation = new Translation();
-
 
   translationForm = new FormGroup({
     language: new FormControl(''),
     field: new FormControl(this.valueTranslation[0]),
-    value: new FormControl('')
-  })
+    value: new FormControl(''),
+  });
 
-  constructor(private http: HttpService) {
+  constructor(private http: HttpService) {}
 
-  }
-
-  ngOnInit(){
+  ngOnInit() {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     console.log(this.flg_isEditable);
-    if(this.type!='item')
-    {
+    if (this.type != 'item') {
       this.translationForm.get('language')!.disabled;
     }
-
   }
   toggle(): void {
     if (this.flg_isEditable) {
@@ -76,15 +69,14 @@ export class TranslationsEditorComponent {
       this.translationForm.get('language')!.value!,
       this.translationForm.get('field')!.value!,
       this.translationForm.get('value')!.value!
-
     );
   }
 
-  ResetTranslationFromForm(){
+  ResetTranslationFromForm() {
     this.translationForm.reset({
       language: '',
       field: this.valueTranslation[0],
-      value: ''
+      value: '',
     });
   }
   UpdateTranslation(translation: Translation) {
@@ -117,47 +109,42 @@ export class TranslationsEditorComponent {
         break;
     }
   }
-    InsertTranslation() {
-      if (this.translationForm.get('language')!.value! != null && this.translationForm.get('value')!.value! != null)
-      {
-        switch (this.type) {
-          case 'Item':
-            this.http
-              .InsertItemTranslation(this.id, this.GetTranslationFromForm())
-              .subscribe((data) => {
-                this.translations = data;
-                this.newTranslation = new Translation();
-                this.ResetTranslationFromForm();
-              });
-              break
+  InsertTranslation() {
+    if (
+      this.translationForm.get('language')!.value! != null &&
+      this.translationForm.get('value')!.value! != null
+    ) {
+      switch (this.type) {
+        case 'Item':
+          this.http
+            .InsertItemTranslation(this.id, this.GetTranslationFromForm())
+            .subscribe((data) => {
+              this.translations = data;
+              this.newTranslation = new Translation();
+              this.ResetTranslationFromForm();
+            });
+          break;
 
-              case 'Category':
-            this.http
-              .InsertCategoryTranslation(this.id, this.GetTranslationFromForm())
-              .subscribe((data) => {
-                this.translations = data;
-                this.newTranslation = new Translation();
-                this.ResetTranslationFromForm();
+        case 'Category':
+          this.http
+            .InsertCategoryTranslation(this.id, this.GetTranslationFromForm())
+            .subscribe((data) => {
+              this.translations = data;
+              this.newTranslation = new Translation();
+              this.ResetTranslationFromForm();
+            });
+          break;
 
-
-              });
-              break
-
-              case 'Option':
-            this.http
-              .InsertOptionTranslation(this.id, this.GetTranslationFromForm())
-              .subscribe((data) => {
-                this.translations = data;
-                this.newTranslation = new Translation();
-                this.ResetTranslationFromForm();
-
-
-              });
-              break
-          }
-        }
+        case 'Option':
+          this.http
+            .InsertOptionTranslation(this.id, this.GetTranslationFromForm())
+            .subscribe((data) => {
+              this.translations = data;
+              this.newTranslation = new Translation();
+              this.ResetTranslationFromForm();
+            });
+          break;
       }
-
+    }
   }
-
-
+}

2f3e3ee9eba1114ad3f5cef070cb2506995f3d36
michaellaros <56259544+michaellaros@users.noreply.github.com>
2023-07-07 15:34:27 +0200

    fix error handler httpS moved to interceptor

--- TP_ItemManager/src/app/Services/auth-services/error.interceptor.ts
+++ TP_ItemManager/src/app/Services/auth-services/error.interceptor.ts
@@ -33,7 +33,7 @@ export class ErrorInterceptor implements HttpInterceptor {
         }
 
         const error = err.error.message || err.statusText;
-        return throwError(error);
+        return throwError(() => new Error(error.message || 'Server error!'))
       })
     );
   }
--- TP_ItemManager/src/app/Services/http.service.ts
+++ TP_ItemManager/src/app/Services/http.service.ts
@@ -50,22 +50,22 @@ export class HttpService {
           .append('id', this.id)
           .append('name', this.name),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   FilterCategory(filter: CategoryFilterModel) {
     return this.http
       .post<any>(this.urlAPI + 'Categories', filter)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   FilterOption(filter: OptionFilterModel) {
     return this.http
       .post<any>(this.urlAPI + 'Options', filter)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   FilterKiosk(filter: any) {
     return this.http
       .post<any>(this.urlAPI + 'Kiosks', filter)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   GetItem(id: string) {
@@ -73,7 +73,7 @@ export class HttpService {
       .get<Item>(this.urlAPI + 'Item', {
         params: new HttpParams().append('id', id),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   GetCategory(id: string) {
@@ -81,7 +81,7 @@ export class HttpService {
       .get<Category>(this.urlAPI + 'Category', {
         params: new HttpParams().append('id', id),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   GetOption(id: string) {
@@ -89,7 +89,7 @@ export class HttpService {
       .get<Option>(this.urlAPI + 'Option', {
         params: new HttpParams().append('id', id),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   GetKiosk(id: string) {
@@ -97,54 +97,52 @@ export class HttpService {
       .get<Kiosk>(this.urlAPI + 'Kiosk', {
         params: new HttpParams().append('id', id),
       })
-      // .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+      //
   }
 
   InsertCategory(category: Category) {
     console.log(category);
     return this.http
       .post<Category>(this.urlAPI + 'InsertCategory', category)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateCategory(category: Category) {
     console.log(category);
     return this.http
       .post<Category>(this.urlAPI + 'UpdateCategory', category)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   InsertItem(item: Item) {
     console.log(item);
     return this.http
       .post<Category>(this.urlAPI + 'InsertItem', item)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateItem(item: Item) {
     console.log(item);
     return this.http
       .post<Category>(this.urlAPI + 'UpdateItem', item)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   InsertOption(option: Option) {
     console.log(option);
     return this.http
       .post<Category>(this.urlAPI + 'InsertOption', option)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateOption(option: Option) {
     console.log(option);
     return this.http
       .post<Category>(this.urlAPI + 'UpdateOption', option)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
-  ErrorHandler(error: HttpErrorResponse) {
-    return throwError(() => new Error(error.message || 'Server error!'));
-  }
+
 
 
   InsertItemTranslation(id: string, translation: Translation) {
@@ -153,7 +151,7 @@ export class HttpService {
       .post<Translation[]>(this.urlAPI + 'InsertItemTranslation', translation, {
         params: new HttpParams().append('id', id),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateItemTranslation(id: string, translation: Translation) {
@@ -162,7 +160,7 @@ export class HttpService {
       .post<Translation[]>(this.urlAPI + 'UpdateItemTranslation', translation, {
         params: new HttpParams().append('id', id),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   InsertCategoryTranslation(id: string, translation: Translation) {
@@ -175,7 +173,7 @@ export class HttpService {
           params: new HttpParams().append('id', id),
         }
       )
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateCategoryTranslation(id: string, translation: Translation) {
@@ -188,7 +186,7 @@ export class HttpService {
           params: new HttpParams().append('id', id),
         }
       )
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   InsertOptionTranslation(id: string, translation: Translation) {
@@ -201,7 +199,7 @@ export class HttpService {
           params: new HttpParams().append('id', id),
         }
       )
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateOptionTranslation(id: string, translation: Translation) {
@@ -214,27 +212,27 @@ export class HttpService {
           params: new HttpParams().append('id', id),
         }
       )
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   InsertAssignedObject(data: any, methodName: string) {
     console.log(data);
     return this.http
       .post<AssignedObject[]>(this.urlAPI + methodName, data)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   UpdateAssignedObject(data: any, methodName: string) {
     console.log(data);
     return this.http
       .post<AssignedObject[]>(this.urlAPI + methodName, data)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   DeleteAssignedObject(data: any, methodName: string) {
     console.log(data);
     return this.http
       .post<AssignedObject[]>(this.urlAPI + methodName, data)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   DeleteObject(type: string, id: string) {
@@ -242,7 +240,7 @@ export class HttpService {
       .post<AssignedObject[]>(this.urlAPI + 'Delete' + type, null, {
         params: new HttpParams().append('id', id),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateTimespan(
@@ -263,7 +261,7 @@ export class HttpService {
           params: new HttpParams().append('id', idCategory),
         }
       )
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   InsertTimespan(
@@ -279,7 +277,7 @@ export class HttpService {
           params: new HttpParams().append('id', idCategory),
         }
       )
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   DeleteTimespan(timespan_id: string, category_id: string) {
@@ -289,7 +287,7 @@ export class HttpService {
           .append('timespan_id', timespan_id)
           .append('category_id', category_id),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   DeleteTranslation(translation: Translation, id: string, type: string) {
@@ -301,7 +299,7 @@ export class HttpService {
           params: new HttpParams().append('id', id),
         }
       )
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   GetImages(folderName: string) {
@@ -309,7 +307,7 @@ export class HttpService {
       .get<string[]>(this.urlAPI + 'Images', {
         params: new HttpParams().append('folderName', folderName),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UploadImage(folderName: string, image: File) {
@@ -319,63 +317,60 @@ export class HttpService {
       .post<string[]>(this.urlAPI + 'Image', formData, {
         params: new HttpParams().append('folderName', folderName),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   DeleteImage(imageName: string) {
     return this.http
       .post<string[]>(this.urlAPI + 'DeleteImage', null, {
         params: new HttpParams().append('imageName', imageName),
       })
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   InsertKiosk(kiosk: Kiosk) {
     console.log(kiosk);
     return this.http
       .post<Kiosk>(this.urlAPI + 'InsertKiosk', kiosk)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   UpdateKiosk(kiosk: Kiosk) {
     console.log(kiosk);
     return this.http
       .post<Kiosk>(this.urlAPI + 'UpdateKiosk', kiosk)
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   GetLanguages() {
     return this.http
       .get<Language[]>(this.assetsUrl + 'i18n/languages.json')
-      .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   Login(name:string, password:string){
 
     return this.http.post<Token>(this.urlAPI + 'DoLogin', {name,password})
-    .pipe(catchError((error: HttpErrorResponse) => {
-      return this.ErrorHandler(error)}
 
-    ));
   }
   GetUsers(filter: any){
     return this.http.post<any>(this.urlAPI + 'GetUsers', filter);
-    // .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+    //
   }
   CreateUser(name:string,password:string)
   {
     return this.http.post<string>(this.urlAPI + 'CreateUser', {name,password})
-    // .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+    //
   }
   UpdateUser(user:UserModelRequest){
     return this.http.post<UserModelRequest>(this.urlAPI + 'UpdateUser',{id:user.id?.toString(),name:user.name} )
-    .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   GetUser(id:string){
     return this.http.post<UserModelRequest>(this.urlAPI + 'GetUser',{id:id,name:''} )
-    .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
   UpdatePassword(id:string, password:string,oldPassword:string){
     return this.http.post<any>(this.urlAPI + 'UpdatePassword',{id:id,password:password,oldPassword:oldPassword})
-    .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
   GetItemVat(id:string)
@@ -383,7 +378,7 @@ export class HttpService {
     return this.http.post<ItemVat>(this.urlAPI + 'GetItemVat', null,{
       params: new HttpParams().append('id', id)
     })
-    .pipe(catchError((error: HttpErrorResponse) => this.ErrorHandler(error)));
+
   }
 
 }
